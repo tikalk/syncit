a959ebe4549c697c847fd89953b3fba925bb9edd
Avishay Maor <avishay@maor.co>
2022-10-17 16:27:12 +0300

    fix swagger api

--- apps/microservices/auth/src/app/auth.controller.ts
+++ apps/microservices/auth/src/app/auth.controller.ts
@@ -1,29 +1,80 @@
 import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 
-// Todo: add this for swagger better documentation
-// import { ApiCreatedResponse, ApiHeader, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
-
 import { AuthService } from './auth.service';
+import {
+  ApiBody,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
+import { LoginInput, RegisterInput, UserData } from './auth.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiBody({
+    description: 'Registration params',
+    type: RegisterInput,
+  })
+  @ApiCreatedResponse({
+    description: 'User registered successfully and cookie saved on initiator.',
+    type: RegisterInput,
+  })
+  @ApiUnprocessableEntityResponse({
+    description: 'Error with the data sent.',
+  })
+  @ApiConflictResponse({
+    description: 'User (email) already exist.',
+  })
   async register(@Req() request: Request, @Res() response: Response) {
     return this.authService.register(request, response);
   }
 
   @Post('login')
+  @ApiBody({
+    description: 'Login params',
+    type: LoginInput,
+  })
+  @ApiCreatedResponse({
+    description: 'User logged in successfully and cookie saved on initiator.',
+  })
+  @ApiUnprocessableEntityResponse({
+    description: 'Error with the data sent.',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Error with the data sent. Unauthorized',
+  })
   async login(@Req() request: Request, @Res() response: Response) {
     return this.authService.login(request, response);
   }
+
   @Post('logout')
+  @ApiOkResponse({
+    description:
+      'User successfully logged out and cookie removed from initiator.',
+  })
   async logout(@Req() request: Request, @Res() response: Response) {
     return this.authService.logout(request, response);
   }
 
   @Get('me')
+  @ApiOkResponse({
+    description: 'Returns user data if logged in',
+    type: UserData,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized',
+  })
+  @ApiForbiddenResponse({
+    description: 'Unauthorized - Blocked by auth guard',
+  })
   async me(@Req() request: Request, @Res() response: Response) {
     return this.authService.me(request, response);
   }
--- apps/microservices/auth/src/main.ts
+++ apps/microservices/auth/src/main.ts
@@ -22,7 +22,6 @@ async function bootstrap() {
     .setTitle('Auth service')
     .setDescription('The auth service API description')
     .setVersion('1.0')
-    .addTag('auth')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
