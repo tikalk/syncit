4605c8c4359e19089b4e8a963ad1f8b8df632b88
Najeeb Shamlih <n-sham@users.noreply.github.com>
2022-11-22 13:39:58 +0200

    init integrations microservice (#71)

--- apps/microservices/integrations/jest.config.ts
+++ apps/microservices/integrations/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'be',
-  preset: '../../jest.preset.js',
+  displayName: 'microservices-integrations',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/be',
+  coverageDirectory: '../../../coverage/apps/microservices/integrations',
 };
